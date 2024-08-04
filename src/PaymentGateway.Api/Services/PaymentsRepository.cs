edfdeb4beb5e2eb0c6b30aef9f9e2eed0e60e1be
Mohannad Zeido <mohannad.zeido@gmail.com>
2024-08-04 22:42:44 +0100

    Update PaymentRepository to store payment

--- src/PaymentGateway.Api/Services/PaymentsRepository.cs
+++ src/PaymentGateway.Api/Services/PaymentsRepository.cs
@@ -1,17 +1,33 @@
-﻿using PaymentGateway.Api.Models.Responses;
+﻿using PaymentGateway.Domain;
+using PaymentGateway.Domain.Models;
 
 namespace PaymentGateway.Api.Services;
 
 public class PaymentsRepository
 {
-    private readonly List<PostPaymentResponse> _payments = new();
+    private readonly List<ProcessedPayment> _payments = new();
     
-    public void Add(PostPaymentResponse payment)
+    public Guid Add(ProcessPaymentRequest payment, ProcessPaymentResponse paymentResult)
     {
-        _payments.Add(payment);
+        var paymentToStore = new ProcessedPayment
+        {
+            Amount = payment.Amount,
+            Currency = payment.Currency,
+            Cvv = payment.Cvv,
+            Id = Guid.NewGuid(),
+            CardNumberLastFourDigits = CardNumberMasker.GetLastFourDigits(payment.CardNumber),
+            ExpiryMonth = payment.ExpiryMonth,
+            ExpiryYear = payment.ExpiryYear,
+            PaymentStatus = paymentResult.PaymentStatus,
+            AuthorisationCode = paymentResult.AuthorizationCode
+        };
+            
+        _payments.Add(paymentToStore);
+
+        return paymentToStore.Id;
     }
 
-    public PostPaymentResponse? Get(Guid id)
+    public ProcessedPayment? Get(Guid id)
     {
         return _payments.FirstOrDefault(p => p.Id == id);
     }
